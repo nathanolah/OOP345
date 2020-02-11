d692ce640b8440e647a1b82ad8c78c2ab322455b
Nathan Olah <nolah@myseneca.ca>
2020-02-11 12:18:23 -0500

    WS04 in_lab

---
+++ WS04/in_lab/w4/Reservation.h
@@ -0,0 +1,28 @@
+// Name: Nathan Olah 
+// Seneca Student ID: 124723198
+// Seneca email: nolah@myseneca.ca
+// Date of completion: 2020/02/03
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+
+#ifndef SDDS_RESERVATION_H__
+#define SDDS_RESERVATION_H__
+#include <iostream>
+namespace sdds {
+   
+   class Reservation {
+      std::string m_reservationId;
+      std::string m_name;
+      std::string m_email;
+      size_t m_numberOfPeople;
+      size_t m_day;
+      size_t m_hour;
+   public:
+      Reservation();
+      Reservation(const std::string& res);
+      void display(const char* cString);
+      friend std::ostream& operator<<(std::ostream& os, Reservation& res);
+   };
+}
+#endif // !SDDS_RESERVATION_H__
