e69cf9febf54aca50ca941ad1ba09ccb4221e90d
Nathan Olah <nolah@myseneca.ca>
2020-03-10 19:59:16 -0400

    Fixed time

--- WS07/in_lab/w7/SongCollection.cpp
+++ WS07/in_lab/w7/SongCollection.cpp
@@ -53,7 +53,6 @@ namespace sdds {
    SongCollection::SongCollection() {}
    SongCollection::~SongCollection() {
       while (!m_songs.empty()) {
-        // delete m_songs.back();
          m_songs.pop_back();
       }
    }
@@ -85,29 +84,10 @@ namespace sdds {
 
             newSong.m_songLength = getString(tempStr, true);
             trim(newSong.m_songLength);
-            newSong.m_songLength.insert(1, 1, ':');
+            getTime(newSong.m_songLength);
 
             if (stod(tempStr))
                newSong.m_price = stod(getString(tempStr, true));
-
-            // THIS WAS WHEN I WAS TRYING TO USE Song DYNAMICALLY
-            /*
-            newSong->m_title = getString(tempStr, false);
-            trim(newSong->m_title);
-            newSong->m_artist = getString(tempStr, false);
-            trim(newSong->m_artist);
-            newSong->m_album = getString(tempStr, false);
-            trim(newSong->m_album);
-
-            newSong->m_yearOfRelease = getString(tempStr, true);
-            trim(newSong->m_yearOfRelease);
-
-            newSong->m_songLength = getString(tempStr, true);
-            trim(newSong->m_songLength);
-            newSong->m_songLength.insert(1, 1, ':');
-
-            if (stod(tempStr))
-               newSong->m_price = stod(getString(tempStr, true));*/
 
             // Add song to m_songs
             m_songs.push_back(newSong);
@@ -117,21 +97,61 @@ namespace sdds {
       file.close();
    }
 
+   std::string SongCollection::checkTime(size_t seconds) {
+      string str;
+      if (seconds < 10) {
+         str = to_string(seconds);
+         str.insert(0, "0");
+      }
+      else {
+         str = to_string(seconds);
+      }
+      return str;
+   }
+
+   // Gets time from seconds
+   void SongCollection::getTime(std::string& str) {
+      size_t seconds = 0;
+      size_t minutes = 0;
+      size_t hours = 0;
+
+      seconds = stoi(str);
+
+      minutes += (seconds / 60);
+      hours += (minutes / 60);
+      minutes %= 60;
+      seconds %= 60;
+      hours %= 60;
+
+      if (hours != 0) {
+         str = to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+      }
+      else {
+         str = to_string(minutes) + ":" + checkTime(seconds);
+      }
+   }
+
    // Iterate through the collection 
    void SongCollection::display(std::ostream& out)const {
-      //std::for_each(m_songs.begin(), m_songs.end(), [&](const Song* song) { out << *song << endl; });
-      std::for_each(m_songs.begin(), m_songs.end(), [&](const Song song) { out << song << endl; });
+      std::for_each(m_songs.begin(), m_songs.end(), [&](const Song song) {
+         if (song.m_title == m_songs.back().m_title) {
+            out << song;
+         }
+         else {
+            out << song << endl;
+         }
+      });
 
    }
 
    std::ostream& operator<<(std::ostream& out, const Song& theSong) {
-      
-      out << "| " << left << setw(20) << theSong.m_title
+      out << "| " << left << setw(20) << setfill(' ') << theSong.m_title
          << " | " << left << setw(15) << theSong.m_artist
          << " | " << left << setw(20) << theSong.m_album
          << " | " << right << setw(6) << theSong.m_yearOfRelease
-         << " | " << theSong.m_songLength
-         << " | " << theSong.m_price << " | ";
+         << " | " << left << setw(4) << theSong.m_songLength
+         << " | " << theSong.m_price << " |";
+ 
       return out;
    }
 }
